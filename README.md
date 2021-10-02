1cd9735c3360705ff897bda1ccd9d244735da875
Oskar Hahn <mail@oshahn.de>
2021-10-02 16:56:44 +0200

    Add skeleton

---
+++ README.md
@@ -0,0 +1,28 @@
+# OpenSlides performance
+
+Tool to test the limits of OpenSlides.
+
+
+## Install
+
+### With installed go
+
+```
+go install github.com/OpenSlides/openslides-performance@latest
+```
+
+
+### With checked out repo
+
+```
+go build
+```
+
+
+## Run
+
+You can see the usage of the command by calling it.
+
+```
+performance
+```
---
+++ cmd/root.go
@@ -0,0 +1,49 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+const rootHelp = `Performance is an helper tool to test the limits of OpenSlies.
+
+Each task is implemented as a subcommand.`
+
+type config struct {
+	domain   string
+	username string
+	password string
+	http     bool
+}
+
+func (c *config) addr() string {
+	proto := "https"
+	if c.http {
+		proto = "http"
+	}
+	return proto + "://" + c.domain
+}
+
+func cmdRoot(cfg *config) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:          "performance",
+		Short:        "performance is a tool that brings OpenSlides to its limit.",
+		Long:         rootHelp,
+		SilenceUsage: true,
+	}
+
+	cmd.PersistentFlags().StringVarP(&cfg.domain, "domain", "d", "localhost:8000", "Domain where to send the requests")
+	cmd.PersistentFlags().StringVarP(&cfg.username, "username", "u", "admin", "Username that can create the users.")
+	cmd.PersistentFlags().StringVarP(&cfg.password, "password", "p", "admin", "Password to use.")
+	cmd.PersistentFlags().BoolVar(&cfg.http, "http", false, "Use http instead of https. Default is https.")
+
+	return cmd
+}
+
+// Execute starts the root command.
+func Execute() error {
+	cfg := new(config)
+	cmd := cmdRoot(cfg)
+	cmd.AddCommand()
+
+	return cmd.Execute()
+}
