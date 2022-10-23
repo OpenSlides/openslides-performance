aeb14a6c18d498eb2243a394a161cdb98b876ea8
Oskar Hahn <mail@oshahn.de>
2022-10-23 11:05:03 +0200

    Use diffrent accounts in connect

--- connect/options.go
+++ connect/options.go
@@ -4,10 +4,11 @@ import "os"
 
 // Options is the meta information for the cli.
 type Options struct {
-	Amount    int      `help:"Amount of connections to use." short:"n" default:"10"`
-	Body      string   `help:"Request Body." short:"b"`
-	BodyFile  *os.File `help:"Request Body from a file. Use - for stdin"`
-	SkipFirst bool     `help:"Use skip first flag to save traffic."`
+	Amount          int      `help:"Amount of connections to use." short:"n" default:"10"`
+	Body            string   `help:"Request Body." short:"b"`
+	BodyFile        *os.File `help:"Request Body from a file. Use - for stdin"`
+	SkipFirst       bool     `help:"Use skip first flag to save traffic."`
+	MuliUserMeeting int      `help:"Use dummy user accounts from meeting. 0 For global dummys. Uses the same account as default." short:"m" default:"-1"`
 }
 
 // Help returns the help message
@@ -18,5 +19,11 @@ this change.
 
 Example:
 
-openslides-performance connect -b '[{"collection":"organization","ids":[1],"fields":{"committee_ids":{"type":"relation-list","collection":"committee","fields":{"name":null}}}}]'`
+openslides-performance connect -b '[{"collection":"organization","ids":[1],"fields":{"committee_ids":{"type":"relation-list","collection":"committee","fields":{"name":null}}}}]'
+
+With --muli-user-meeeting each connection uses a different user account. 
+The accounts can be created with the "create-users" command. The attribute 
+needs the meeting id that was used to create the users. Use "0" when the 
+users where created without a meeting.
+`
 }
