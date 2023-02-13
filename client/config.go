bc9c4b1feabd2c9e9c5ca006c19bd8834a5b62dd
Oskar Hahn <mail@oshahn.de>
2023-02-13 11:05:31 +0100

    Use superadmin as default user
    It is the default in the manage-service

--- client/config.go
+++ client/config.go
@@ -3,8 +3,8 @@ package client
 // Config for all commands.
 type Config struct {
 	Domain   string `help:"Domain of the OpenSlides server to probe." short:"d" default:"localhost:8000"`
-	Username string `help:"Username for logged-in requests." short:"u" default:"admin"`
-	Password string `help:"Password to use for logged-in requests." short:"p" default:"admin"`
+	Username string `help:"Username for logged-in requests." short:"u" default:"superadmin"`
+	Password string `help:"Password to use for logged-in requests." short:"p" default:"superadmin"`
 	HTTP     bool   `help:"Use http instead of https. Default is https."`
 	IPv4     bool   `help:"Force IPv4 for requests." short:"4"`
 	FakeAuth bool   `help:"Do not login but expect user id 1."`
