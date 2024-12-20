4f917e9e32c3ed134a59928ca7672ef8012fa9a2
rrenkert <raimund.renkert@intevation.de>
2024-12-20 10:58:57 +0100

    Allow file for backenaction data payload.

--- backendaction/options.go
+++ backendaction/options.go
@@ -1,10 +1,13 @@
 package backendaction
 
+import "os"
+
 // Options is the meta information for the cli.
 type Options struct {
-	Action  string `arg:"" help:"Name of the action."`
-	Amount  int    `help:"Amount of action to be called." short:"n" default:"10"`
-	Content string `arg:"" help:"content of the action."`
+	Action   string `arg:"" help:"Name of the action."`
+	Amount   int    `help:"Amount of action to be called." short:"n" default:"10"`
+	Content  string `help:"content of the action." short:"c" default:""`
+	BodyFile *os.File `help:"File containing the 'data' array of action payload sent to the backend" short:"b"`
 }
 
 // Help returns the help message
@@ -19,5 +22,5 @@ If content is "-", then the content is read from stdin.
 
 Example:
 
-openslides-performance backend-action motion.create '{"meeting_id":1,"text":"hello world","title":"motion\u"}'`
+openslides-performance backend-action motion.create -c '{"meeting_id":1,"text":"hello world","title":"motion\u"}'`
 }
